5c5ccf9e0ff5fbcaa6f40536909eb9a92676c8ec
Ericcentrik <enaiman1@gmail.com>
2020-07-24 13:36:15 -0400

    most of logic is built out and connected scripts has imported linked_list and blossom_lib

--- script.py
+++ script.py
@@ -1,7 +1,10 @@
+from linked_list import Node, LinkedList
+from blossom_lib import flower_definitions
+
 class HashMap:
   def __init__(self, size):
     self.array_size = size
-    self.array = [None for number in range(size)]
+    self.array = [LinkedList() for number in range(size)]
   
   def hash(self, key):
     return sum(key.encode())
@@ -11,13 +14,29 @@ class HashMap:
 
   def assign(self,key, value):
     array_index = self.compress(self.hash(key))
-    self.array[array_index] = [key, value]
+    # self.array[array_index] = [key, value]
+    payload = Node([key, value])
+    list_at_array = self.array[array_index]
+    for item in list_at_array:
+      if key == item[0]:
+        item[1] = value
+        return
+    list_at_array.insert(payload)
 
-  def retrieve(self, key)
+  def retrieve(self, key):
     array_index = self.compress(self.hash(key))
-    payload = self.array[array_index]
-    if payload != None:
-      return payload[1]
-    else:
-      return None
+    list_at_index = self.array[array_index]
+    for item in list_at_index:
+      if key == item[0]:
+        return item[1]
+    return None
+
+blossom = HashMap(len(flower_definitions))
+
+for flower in flower_definitions:
+  blossom.assign(flower[0], flower[1])
+
+print(blossom.retrieve("daisy"))
   
+
+
